b8434847112aaedc876931735cb8462ca31b2a81
Glitch (p5-js-template-cssi-2020) <none>
2020-07-05 21:47:31 +0000

    🍙🌒 Checkpoint ./script.js:18530910/4683

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
-// Add to this list as you consult the p5.js documentation for other functions. 
+// Add to this list as you consult the p5.js documentation for other functions.
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
           keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
 let backgroundColor;
+let circle = {
+  
+};
 
 function setup() {
   // Canvas & color settings
   createCanvas(400, 400);
   colorMode(HSB, 360, 100, 100);
-  backgroundColor = 95;
+  backgroundColor = 100;
 }
 
 function draw() {
-  background(backgroundColor);
+  background(75, 100, 100);
+  fill(0);
+  ellipse(100, 100, 100);
 }
