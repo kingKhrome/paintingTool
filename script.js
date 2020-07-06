79a0d42b98bde38a168d8816e3dd7263010ddfab
Glitch (oval-jeweled-raptorex) <none>
2020-07-06 17:33:07 +0000

    👭🎢 Checkpoint

--- script.js
+++ script.js
@@ -7,51 +7,10 @@
 
 function setup() {
   createCanvas(300, 300);
-  angleMode(DEGREES);
 }
 
 function draw() {
   background(240);
-
-  // Brush settings
-  noFill();
-  strokeWeight(5);
-
-  // Ring 1: Blue
-  stroke(10, 134, 205);
-  ellipse(50, 50, 50);
-
-  // Ring 2: Yellow
-  stroke(255, 214, 0);
-  ellipse(80, 80, 50);
-
-  // Ring 3: Black
-  stroke(0, 0, 0);
-  ellipse(110, 50, 50);
-
-  // Ring 4: Green
-  stroke(33, 176, 76);
-  ellipse(140, 80, 50);
-
-  // Ring 5: Red
-  stroke(234, 30, 35);
-  ellipse(170, 50, 50);
-
-  /* Overlapping sections */
-
-  // Ring 1: Blue
-  stroke(10, 134, 205);
-  arc(50, 50, 50, 50, 0, 30);
-
-  // Ring 2: Yellow
-  stroke(255, 214, 0);
-  arc(80, 80, 50, 50, 270, 300);
-
-  // Ring 3: Black
-  stroke(0, 0, 0);
-  arc(110, 50, 50, 50, 0, 30);
-
-  // Ring 4: Green
-  stroke(33, 176, 76);
-  arc(140, 80, 50, 50, 270, 300);
+  
+  ellipse(100, 200, 50);
 }
