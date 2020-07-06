a071d5ba1fdc9d762df6546838f5c1c61597c321
Glitch (oval-jeweled-raptorex) <none>
2020-07-06 02:54:21 +0000

    🛥🐛 Checkpoint ./index.html:18530910/31 ./script.js:18530910/2142

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
 // Add to this list as you consult the p5.js documentation for other functions.
-/* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
+/* global clear, createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
           keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
@@ -14,15 +14,16 @@ const circle = {
 
 function setup() {
   // Canvas & color settings
-  const canvasDiv = document.getElementById('canvas');
+  const canvasDiv = document.getElementById("canvas");
   const canvas = createCanvas(canvasDiv.offsetWidth, 400);
-  canvas.parent('canvas');
+  canvas.parent("canvas");
   colorMode(HSB, 360, 100, 100);
   backgroundColor = 100;
 }
 
 function draw() {
-  background(75, 100, 100);
+  clear();
+  // background(75, 100, 100, 0.5);
   fill(0);
   ellipse(circle.x, circle.y, circle.d);
   if (!mouseIsPressed) {
