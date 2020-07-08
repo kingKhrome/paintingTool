79c1518cac53a72ff519069c9bafc72d6bb25f28
Glitch (oval-jeweled-raptorex) <none>
2020-07-08 22:28:22 +0000

    😄👾 Checkpoint ./script.js:18588164/3395

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-// Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
-// Add to this list as you consult the p5.js documentation for other functions.
-/* global angleMode, DEGREES, arc, clear, createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
-          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, tint, noTint
-          mouseX, mouseY, strokeWeight, line, mouseIsPressed, noFill, windowWidth, windowHeight, noStroke, 
-          keyCode, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
-
-// We'll use variables for most of our colors in this code-along.
-
+let backgroundColor
 let width = 500
 let height = 500
 function setup() {
   // Canvas & color settings
-  createCanvas(400, 400);
+  createCanvas(width, height);
+  colorMode(HSB,360,100,100)
 
 }
 
-paintBox_X = [0,1*width/10,2*width/10,3*width/10,4*width/10,5*width/10,6*width/10,7*width/10,8*width/10,9*width/10,width]
-paintBox_Y = height/25
-for(let i=0; i<array.length; i++) {
-  
-}
+let paintBox_X = [0,1*width/10,2*width/10,3*width/10,4*width/10,5*width/10,6*width/10,7*width/10,8*width/10,9*width/10,width]
+let paintBox_Y = height/25
+let color = 1
+
+
 
 function draw() {
   background(backgroundColor);
+  for(let i=0; i<paintBox_X.length-1; i++){ 
+    fill(color*i*35,100,100)
+    rect( paintBox_X[i] ,paintBox_Y ,paintBox_X[i+1] ,paintBox_Y) }
+  console.log('hello')
+  }
+             
   
-}
+  
 
